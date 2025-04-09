e95a29312cd058e778dab7eac8ba8c8a4c9ce797
Jimmy <jmarchel100@gmail.com>
2025-04-09 16:35:42 +0700

    mod, test main dll

--- src/chapter4/mod.rs
+++ src/chapter4/mod.rs
@@ -1,6 +1,8 @@
 pub mod base_converter;
 pub mod divide_by_two;
+pub mod infix_to_postfix;
 pub mod par_checker1;
 pub mod par_checker2;
 pub mod par_checker3;
+pub mod postfix_eval;
 pub mod stack;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use chapter4::{base_converter::base_converter, divide_by_two::divide_by_two};
+use chapter4::postfix_eval::postfix_eval;
 
 mod chapter4;
 
 fn main() {
-    let num1 = 10;
-    let num2 = 43;
-    let bin_str = base_converter(num1, 2);
-    let hex_str = base_converter(num2, 16);
-    println!("{num1} : b{bin_str}, {num2} : x{hex_str}")
+    let postfix = "1 2 + 1 2 + *";
+    let res = postfix_eval(postfix);
+    match res {
+        Some(val) => println!("res = {val}"),
+        None => println!("{postfix} isn't a valid prefix"),
+    }
 }
