7d2305a837200c9e996123e8bf2ac43d0ecea33e
JiMarchel <jmarchel100@gmail.com>
2025-05-10 19:43:37 +0700

    change name dir chapter4 to basic_data_algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use chapter4::dequeue::palindrome_checker::palindrome_checker;
-
-mod chapter4;
-
-fn main() {
-    let pal = "rustsur";
-    let is_pal = palindrome_checker(pal);
-    println!("{pal} is palindrome string: {is_pal}");
-
-    let pal = "panda";
-    let is_pal = palindrome_checker(pal);
-    println!("{pal} is palindrome string: {is_pal}");
-}
+mod basic_data_structure;
+mod searching;
+fn main() {}
