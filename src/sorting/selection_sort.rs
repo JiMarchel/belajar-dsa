90dde2c1fb6f5ef2cf215a10bac741423022c5c4
JiMarchel <jmarchel100@gmail.com>
2025-05-13 22:17:20 +0700

    selection sort

---
+++ src/sorting/selection_sort.rs
@@ -0,0 +1,13 @@
+pub fn selection_sort(nums: &mut Vec<i32>) {
+    let mut left = nums.len() - 1; //left marker
+    while left > 0 {
+        let mut pos_max = 0;
+        for i in 1..=left {
+            if nums[i] > nums[pos_max] {
+                pos_max = i;
+            }
+        }
+        nums.swap(left, pos_max);
+        left -= 1;
+    }
+}
