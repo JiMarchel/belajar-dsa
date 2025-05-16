0ae31780896b547ddde7b021c32806424c3d8f84
JiMarchel <jmarchel100@gmail.com>
2025-05-16 22:09:19 +0700

    macro mod

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use searching::interpolation_search::interpolation_search;
 
 mod basic_data_structure;
+mod learn_macro;
 mod searching;
 mod sorting;
+
 fn main() {
     let wk = [1, 9, 10, 15, 16, 17, 19, 23, 27, 28, 29, 30, 32, 35];
     println!("{}", interpolation_search(&wk, 30));
